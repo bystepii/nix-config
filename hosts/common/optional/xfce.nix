c8627135ef6952ed060a7a4bd000720e83b763f0
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2026-03-16 11:34:22 +0100

    fix(xfce): enable xserver and lightdm for XFCE session

--- hosts/common/optional/xfce.nix
+++ hosts/common/optional/xfce.nix
@@ -1,5 +1,8 @@
 # NOTE(starter): This is just a basic enabling of the XFCE windows manager for simplicity
 {
+  services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.displayManager.defaultSession = "xfce";
   services.xserver.desktopManager.xfce.enable = true;
   services.xserver.desktopManager.xfce.enableScreensaver = false;
 }
