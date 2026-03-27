62ed87da62502015d766eedcc11ee7bd39b40294
emergentmind <emergentmind@users.noreply.github.com>
2026-03-27 13:35:13 -0600

    feat: enable color-conventional-commits from introdus

--- home/common/core/git.nix
+++ home/common/core/git.nix
@@ -10,6 +10,10 @@
     pkgs.delta # git diff tool
   ];
 
+  introdus.color-conventional-commits = {
+    enable = true;
+  };
+
   programs.git = {
     enable = true;
     package = pkgs.gitFull;
@@ -31,4 +35,11 @@
   };
 
   home.sessionVariables.GIT_EDITOR = osConfig.hostSpec.defaultEditor;
+
+  programs.zsh.shellAliases = {
+    # Copy the last commit id from a branch
+    glc = ''f() { git log --oneline $@ | head -1 | awk "{print \$1}" | wl-copy }; git rev-parse --is-inside-work-tree >/dev/null && f && echo "Copied commit: $(wl-paste)"'';
+    glcf = ''f() { glo $@ | fzf --ansi --preview "git show --color=always {1}" | sed 's/\x1b\[[0-9;]*m//g' | awk "{print \$1}" | wl-copy }; git rev-parse --is-inside-work-tree >/dev/null && f && echo "Copied commit: $(wl-paste)"'';
+  };
+
 }
