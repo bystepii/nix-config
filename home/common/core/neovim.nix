f93df92866097fb5941cba771da6805e98bb00fa
emergentmind <emergentmind@users.noreply.github.com>
2026-03-31 20:02:24 -0600

    refactor(neovim): reference introdus

--- home/common/core/neovim.nix
+++ home/common/core/neovim.nix
@@ -1,8 +1,20 @@
-{ ... }:
+{ lib, ... }:
 {
   introdus.neovim = {
     enable = true;
     fontSize = 14;
     wrapper = "emergentvim";
+  };
+  # My custom neovim wrapper, built on top of the introdus neovim base, is enabled by the above
+  # and exposed in the config as wrappers.neovim.
+  wrappers.neovim = {
+    settings = {
+      # Set impure paths to allow hot reloading of `plugin/`, `snippets/`, etc
+      unwrappedConfig = "/home/ta/src/nix/neovim";
+      baseConfig = lib.mkForce "/home/ta/src/nix/introdus/ta/wrappers/neovim";
+      devMode = true;
+      neovide = true;
+      terminalMode = true;
+    };
   };
 }
