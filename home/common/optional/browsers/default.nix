33fa89c5de252905f36305735c3918476ef92a7d
emergentmind <emergentmind@users.noreply.github.com>
2026-04-08 15:37:30 -0600

    refactor(firefox): WIP move full firefox module to introdus

--- home/common/optional/browsers/default.nix
+++ home/common/optional/browsers/default.nix
@@ -1,4 +1,10 @@
 {
+  introdus.firefox = {
+    # auto-enabled by introdus for hosts where hostSpec.useWindowManager is true
+    profileName = "main";
+    profileID = 0;
+  };
+
   imports = [
     ./brave.nix
     ./chromium.nix
