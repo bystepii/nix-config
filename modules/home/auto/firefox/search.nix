3fbb6e43f20087eb9e2a4e53c9da1eb72e3e94be
emergentmind <emergentmind@users.noreply.github.com>
2026-04-24 09:41:51 -0600

    refactor(firefox): forgot to remove search stuff I don't use

--- modules/home/auto/firefox/search.nix
+++ modules/home/auto/firefox/search.nix
@@ -2,52 +2,14 @@
 {
   default = "ddg";
   engines = {
-    "Awesome Lists" = {
-      urls = [ { template = "https://github.com/search?q=awesome+{searchTerms}&type=repositories"; } ];
-
-      definedAliases = [
-        "@awesome"
-      ];
-    };
-    "crates.io" = {
-      urls = [ { template = "https://crates.io/search?q={searchTerms}"; } ];
-
-      definedAliases = [ "@crates" ];
-    };
-    "Rust Std" = {
-      urls = [ { template = "https://doc.rust-lang.org/std/?search={searchTerms}"; } ];
-
-      definedAliases = [
-        "@rstd"
-      ];
-    };
-    "Rust Language Documentation" = {
-      urls = [ { template = "https://doc.rust-lang.org/std/?search={searchTerms}"; } ];
-
-      definedAliases = [
-        "@rust"
-      ];
-    };
-    "Rust Crates Documentation" = {
-      urls = [ { template = "https://docs.rs/releases/search?query={searchTerms}"; } ];
-
-      definedAliases = [
-        "@drs"
-        "@docsrs"
-      ];
-    };
-    "SearXNG" = {
-      urls = [ { template = "https://search.inetol.net/search?q={searchTerms}"; } ];
-
-      definedAliases = [
-        "@sr"
-        "@searx"
-      ];
-    };
-    "Hanzicraft" = {
-      urls = [ { template = "https://hanzicraft.com/character/{searchTerms}"; } ];
-      definedAliases = [ "@hc" ];
-    };
+    # TODO: Add custom search
+    # "Awesome Lists" = {
+    #   urls = [ { template = "https://github.com/search?q=awesome+{searchTerms}&type=repositories"; } ];
+    #
+    #   definedAliases = [
+    #     "@awesome"
+    #   ];
+    # };
     "wikipedia".metaData.hidden = true;
     "google".metaData.hidden = true;
     "amazondotcom-us".metaData.hidden = true;
