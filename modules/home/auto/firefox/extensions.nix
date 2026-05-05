280c9534d2baaa44410e48898cf2e6834a74d35c
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2026-05-05 19:41:00 +0200

    feat(firefox): add keepassxc-browser extension

--- modules/home/auto/firefox/extensions.nix
+++ modules/home/auto/firefox/extensions.nix
@@ -7,6 +7,7 @@
 # Anything that isn't already part of introdus
 {
   packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
+    keepassxc-browser
   ];
   settings = {
   };
