2aea585df5a1c8d931aaafc6f3185f106a4959ba
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2026-05-06 19:20:11 +0200

    feat(nfs): fix mount

--- hosts/common/optional/nfs-laptop-mounts.nix
+++ hosts/common/optional/nfs-laptop-mounts.nix
@@ -14,29 +14,7 @@
     ];
   };
 
-  fileSystems."/home/stepii/laptop/C" = {
-    device = "stepan-laptop.home:/home/stepii/C";
-    fsType = "nfs";
-    options = [
-      "_netdev"
-      "nofail"
-      "x-systemd.automount"
-      "x-systemd.mount-timeout=1"
-    ];
-  };
-
-  fileSystems."/home/stepii/laptop/D" = {
-    device = "stepan-laptop.home:/home/stepii/D";
-    fsType = "nfs";
-    options = [
-      "_netdev"
-      "nofail"
-      "x-systemd.automount"
-      "x-systemd.mount-timeout=1"
-    ];
-  };
-
-  # Create the base mountpoint directory. C and D will be provided by the first mount.
+  # Ensure the mountpoint directory exists before the automount tries to use it.
   systemd.tmpfiles.rules = [
     "d /home/stepii/laptop 0755 stepii users -"
   ];
