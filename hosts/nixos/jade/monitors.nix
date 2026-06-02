b45a7a2be90fbf5db3d9d11c74aa04e158e8119a
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2026-06-02 08:42:32 +0200

    Fix monitors

--- hosts/nixos/jade/monitors.nix
+++ hosts/nixos/jade/monitors.nix
@@ -6,29 +6,37 @@
   # This uses the nix-config/modules/hosts/common/monitors.nix module which defaults to enabled.
   # Your nix-config/home-manager/<user>/common/optional/desktops/foo.nix WM config should parse and apply these values to its monitor settings
   #
-  #  ------   ------
-  # | DP-5 | | DP-6 |
-  #  ------   ------
+  #  ------   --------   -------
+  # | DP-3 | |  DP-4  | | eDP-1 |
+  #  ------   --------   -------
   monitors = [
     {
-      name = "DP-5";
-      width = 2560;
-      height = 1440;
-      refreshRate = 165;
-      primary = true;
+      name = "DP-3";
+      width = 1920;
+      height = 1080;
+      refreshRate = 60;
       x = 0;
       y = 0;
       scale = 1.0;
     }
     {
-      name = "DP-6";
+      name = "DP-4";
       width = 2560;
       height = 1440;
-      refreshRate = 165;
-      x = 2560;
+      refreshRate = 60;
+      primary = true;
+      x = 1920;
       y = 0;
       scale = 1.0;
-      workspace = "9";
+    }
+    {
+      name = "eDP-1";
+      width = 1920;
+      height = 1080;
+      refreshRate = 144;
+      x = 4480;
+      y = 0;
+      scale = 1.0;
     }
   ];
 }
