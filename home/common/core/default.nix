23442c495b539fd74e3bf77a5f80cca4ae5f6947
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2026-06-03 14:42:00 +0200

    chore: bump to 26.05

--- home/common/core/default.nix
+++ home/common/core/default.nix
@@ -27,7 +27,7 @@
       findutils # find
       jq # json pretty printer and manipulator
       nix-tree # nix package tree viewer
-      neofetch # fancier system info than pfetch
+      fastfetch # fancier system info than pfetch
       ncdu # TUI disk usage
       pciutils
       pfetch # system info
