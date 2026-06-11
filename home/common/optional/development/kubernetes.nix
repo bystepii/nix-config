f79df3088ffe344a74d08427271bc78b11a70850
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2026-06-11 15:05:54 +0200

    Add kubernetes config

--- home/common/optional/development/kubernetes.nix
+++ home/common/optional/development/kubernetes.nix
@@ -2,13 +2,57 @@
 {
   lib,
   pkgs,
+  config,
+  inputs,
+  secrets,
   ...
 }:
+let
+  sopsFolder = (toString inputs.nix-secrets) + "/sops";
+in
 {
   home.packages = lib.attrValues {
     inherit (pkgs)
       kubectl
       kubernetes-helm
       ;
+  };
+
+  sops.secrets = {
+    "kube/ca" = {
+      sopsFile = "${sopsFolder}/shared.yaml";
+    };
+    "kube/clientCert" = {
+      sopsFile = "${sopsFolder}/shared.yaml";
+    };
+    "kube/clientKey" = {
+      sopsFile = "${sopsFolder}/shared.yaml";
+    };
+  };
+
+  sops.templates."kube-config" = {
+    content = ''
+      apiVersion: v1
+      clusters:
+      - cluster:
+          certificate-authority-data: ${config.sops.placeholder."kube/ca"}
+          server: ${secrets.networking.kube.serverUrl}
+        name: kubernetes
+      contexts:
+      - context:
+          cluster: kubernetes
+          user: kubernetes-admin
+        name: kubernetes-admin@kubernetes
+      current-context: kubernetes-admin@kubernetes
+      kind: Config
+      preferences: {}
+      users:
+      - name: kubernetes-admin
+        user:
+          client-certificate-data: ${config.sops.placeholder."kube/clientCert"}
+          client-key-data: ${config.sops.placeholder."kube/clientKey"}
+    '';
+    path = "${config.home.homeDirectory}/.kube/config";
+    mode = "0600";
   };
 }
