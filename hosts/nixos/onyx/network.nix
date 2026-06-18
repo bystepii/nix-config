5e3b658c9e125329a7592ae05266dd17665ee983
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2026-06-18 19:01:16 +0200

    Fix ip routes

--- hosts/nixos/onyx/network.nix
+++ hosts/nixos/onyx/network.nix
@@ -15,22 +15,18 @@ in
   networking = {
     useDHCP = lib.mkDefault true;
     networkmanager.enable = true;
+  };
 
-    networkmanager.dispatcherScripts = lib.optionals (routes != [ ]) [
-      {
-        type = "basic";
-        source = pkgs.writeText "onyx-static-routes" ''
-          IFACE=$1
-          ACTION=$2
-          if [ "$ACTION" = "up" ]; then
-            ${lib.concatMapStringsSep "\n" (route: ''
-              if ${pkgs.iproute2}/bin/ip route get ${route.gateway} > /dev/null 2>&1; then
-                ${pkgs.iproute2}/bin/ip route add ${route.destination} via ${route.gateway} 2>/dev/null || true
-              fi
-            '') routes}
-          fi
-        '';
-      }
-    ];
+  systemd.services."static-routes" = lib.optionalAttrs (routes != [ ]) {
+    description = "Add static routes";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "NetworkManager.service" ];
+    serviceConfig.Type = "oneshot";
+    script = lib.concatMapStringsSep "\n" (route: ''
+      until ${pkgs.iproute2}/bin/ip route get ${route.gateway} > /dev/null 2>&1; do
+        sleep 1
+      done
+      ${pkgs.iproute2}/bin/ip route add ${route.destination} via ${route.gateway} 2>/dev/null || true
+    '') routes;
   };
 }
